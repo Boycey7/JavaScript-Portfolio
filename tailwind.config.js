7a5ddaa0c2fc72ed3bc186464506d6ecddd477c9
Boycey7 <jake.boyce7@hotmail.com>
2023-10-02 21:47:13 +0100

    finished basic portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': '#2d2e32',
+        'custom-teal': '#0ccbb8'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'mulish': ['Mulish', 'sans-serif']
+      },
+      utilities: {
+        '.scroll-smooth': {
+          'scroll-behavior': 'smooth'
+        }
+      }
+    }
   },
+  variants: {},
   plugins: [],
 }
-
